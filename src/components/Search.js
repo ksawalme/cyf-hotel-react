4c9e3d1082167c788ecce3edebb7b5e6c613abb9
ksawalme <ksawalme@gmail.com>
2018-06-23 15:32:04 +0100

    complete exercise 1  and 2

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,11 +10,21 @@ const Search = props => (
         <div className="form-group search-box">
           <label htmlFor="customerId">Customer Id</label>
           <div className="search-row">
-            <input type="text" id="customerId" className="form-control" placeholder="Customer Id" />
+            <input type="text" id="customerId" className="form-control" placeholder="Customer Id(k)" />
             <button className="btn btn-primary fn-submit-name">Search IDs</button>
           </div>
         </div>
       </div>
+      <div className="col">
+        <div className="form-group search-box">
+          <label htmlFor="customerId">Customer Name</label>
+          <div className="search-row">
+            <input type="text" id="customerId" className="form-control" placeholder="Customer Name(k)" />
+            <button className="btn btn-primary fn-submit-name">Search IDs</button>
+          </div>
+        </div>
+      </div>
+      
     </div>
   </div>
 );
